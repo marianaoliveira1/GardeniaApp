901b52b45f2edb3d0cdf42c65bdf054b04468188
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-06-11 11:05:38 -0300

    welcome, login e cadastro

---
+++ app/src/main/java/com/example/gardeniaapp/login_screen.kt
@@ -0,0 +1,11 @@
+package com.example.gardeniaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
